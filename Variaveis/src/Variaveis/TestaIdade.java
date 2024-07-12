ac0cd4edce2caaa87cf598c2d7cc2e721eea5168
dayane.rosa <dayane.rosa@amk-03.AMAKHA.LOCAL>
2024-07-12 18:13:11 -0300

    Aula1 - Variaveis

---
+++ Variaveis/src/Variaveis/TestaIdade.java
@@ -0,0 +1,52 @@
+package Variaveis;
+
+public class TestaIdade {
+	public static void main(String[] args) {
+		int idade = 20;
+		// imprime a idade
+			System.out.println(idade);
+			// gera uma idade no ano seguinte
+			
+			int idadeNovaAnoQueVem = idade + 1;
+			
+			//imprime idade
+			System.out.println(idadeNovaAnoQueVem);
+			
+			double idadeDouble = 46;
+			System.out.println(idadeDouble);
+			
+			double pi = 3.14;
+			System.out.println(pi);
+			
+			boolean verdade = true;
+			System.out.println(verdade);
+			
+			int idade2 = 17;
+			boolean menorDeIdade = idade2 <18;
+			System.out.println("Menor de idade? ,"+menorDeIdade);
+			
+			int i = 5;
+			int j = i;
+			i = i+1;
+			System.out.println(" j = "+j+ " i= "+ i);
+			
+			byte b = 8;
+			System.out.println(b);
+			
+			short shortExemplo = 9;
+			System.out.println(shortExemplo);
+			
+			char charExemplo = 'c';
+			System.out.println(charExemplo);
+			
+			float floatExemplo = 25;
+			System.out.println(floatExemplo);
+			
+			long exemploLong = 25;
+			System.out.println(exemploLong);
+			
+			double exemploDouble = 26;
+			System.out.println(exemploDouble);
+	}
+
+}
---
+++ Variaveis/src/Variaveis/Variaveis.java
@@ -0,0 +1,8 @@
+package Variaveis;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		System.out.println("Oi Mundo");
+	}
+
+}
